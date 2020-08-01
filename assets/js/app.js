64e3ef15c4b9c0a804b756f5a50b09053ca80016
James Mitchell <iamjmitch1990@gmail.com>
2020-08-02 00:49:50 +1000

    eod 31/07/20

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,9 @@ function qs(elmt) {
 function qsa(elmt) {
   return document.querySelectorAll(elmt);
 }
+
+var head = qs('#fullLogoContainer');
+var jumbo = qs('#hero');
+var windowHeight = window.innerHeight;
+
+jumbo.style.height = ((windowHeight - head.offsetHeight) -  + 'px';
