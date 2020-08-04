e955103c5d3952439fd61dacca42bf7f861eb26b
James Mitchell <iamjmitch1990@gmail.com>
2020-08-04 22:43:46 +1000

    nav shrink added

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,23 @@ function qs(elmt) {
 function qsa(elmt) {
   return document.querySelectorAll(elmt);
 }
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = qs('nav');
+var smallLogo = qs('.hidden.left');
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add('sticky');
+    smallLogo.classList.add('showMiniLogo');
+    smallLogo.classList.remove('hidden');
+  } else {
+    navbar.classList.remove('sticky');
+    smallLogo.classList.remove('showMiniLogo');
+    smallLogo.classList.add('hidden');
+  }
+}
