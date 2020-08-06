d1019add790d8398d92f4a6fd655a0575b6508bd
James Mitchell <iamjmitch1990@gmail.com>
2020-08-07 03:12:32 +1000

    eod 06/08/20

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,30 @@
 function qs(elmt) {
   return document.querySelector(elmt);
 }
-
 function qsa(elmt) {
   return document.querySelectorAll(elmt);
 }
 
 window.onscroll = function () {
-  myFunction();
+  navChange();
 };
 
 var navbar = qs('nav');
 var smallLogo = qs('.hidden.left');
 var sticky = navbar.offsetTop;
+const BTTbtn = qs('#backToTop');
 
-function myFunction() {
+function navChange() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add('sticky');
     smallLogo.classList.add('showMiniLogo');
     smallLogo.classList.remove('hidden');
+    BTTbtn.style.opacity = '1';
   } else {
     navbar.classList.remove('sticky');
     smallLogo.classList.remove('showMiniLogo');
     smallLogo.classList.add('hidden');
+    BTTbtn.style.opacity = '0';
   }
 }
 
@@ -35,3 +37,5 @@ function initMap() {
   });
   var marker = new google.maps.Marker({ position: coordinates, map: map });
 }
+
+//Back to Top Button
