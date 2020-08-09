5f8abf72e9c49734d9433a7634e89c0f89ad0ec0
James Mitchell <iamjmitch1990@gmail.com>
2020-08-10 00:07:49 +1000

    pages added

---
+++ our-work.php
@@ -0,0 +1,11 @@
+<?php include './inc/head.inc';?>
+  <body>
+    <div id="fullPage">
+      
+      <?php include './inc/header.inc';?>
+      
+      <?php include './inc/footer.inc';?>
+    </div>
+    <script src="./assets/js/app.js"></script>      
+  </body>
+</html>
