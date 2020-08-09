434fd70bc0586cf12fa3c85c48a61308adcb7cdf
James Mitchell <iamjmitch1990@gmail.com>
2020-08-09 21:36:05 +1000

    rebase to php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include './inc/head.inc';?>
+  <body>
+    <div id="fullPage">
+      <div id="landing">
+      <?php include './inc/header.inc';?>
+      <?php include './inc/hero.inc';?>
+      </div>
+      <?php include './inc/whyChooseUs.inc';?>
+      <?php include './inc/banner1.inc';?>
+      <?php include './inc/services.inc';?>
+      <?php include './inc/banner2.inc';?>
+      <?php include './inc/visitUs.inc';?>
+      <?php include './inc/footer.inc';?>
+    </div>
+    <script src="./assets/js/app.js"></script>
+    <script src="./assets/js/maps.js"></script>
+    <script src="http://connect.facebook.net/fr_FR/all.js"></script> 
+  </body>
+</html>
